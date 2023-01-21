7c77d7352f0616704ce60034243cf7f57bb0842b
Nu55k0pF <madironbrain@googlemail.com>
2023-01-21 22:06:56 +0100

    Finished the c++ app succesfully!

--- Odd Or Even/odd_or_even.cpp
+++ Odd Or Even/odd_or_even.cpp
@@ -14,14 +14,40 @@ Extras:
 */
 
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main(){
 
     int u_number, divident, divisor;
 
-    cout << 'Give me a randome number.';
+    cout << "Give me a randome number.";
     cin >> u_number;
+    string u_number_str = to_string(u_number);
     
+    if (u_number % 2 == 0 && u_number % 4 == 0){
+        cout << u_number_str + " is a multiple of 4.\n";
+    }
+    else if (u_number % 2 == 0)
+    {
+       cout << u_number_str + " is an even number.\n";
+    }
+    else
+    {
+        cout << u_number_str + " is an odd number.\n";
+    }
 
+    cout << "Now give me a different number to devide:\n";
+    cin >> divident;
+    cout << "Now give me a number to devide by:\n";
+    cin >> divisor;
+
+    if (divident % divisor == 0){
+        cout << "Your numbers devide evenly.\n";
+    }
+    else
+    {
+        cout << "Your numbers don't devide evenly.\n";
+    }
+    
 }
