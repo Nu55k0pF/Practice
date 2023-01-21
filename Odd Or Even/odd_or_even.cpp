bfb7932238cdc29b7019f80bde25e8af5a2e606b
Nu55k0pF <madironbrain@googlemail.com>
2023-01-21 20:27:34 +0100

    Started working on c++ file of exercise 2

---
+++ Odd Or Even/odd_or_even.cpp
@@ -0,0 +1,27 @@
+/*
+Exercise 2 (and Solution)
+
+The exercise comes first (with a few extras if you want the extra challenge or want to spend more time), followed by a discussion. Enjoy!
+
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. 
+Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+    1. If the number is a multiple of 4, print out a different message.
+    2. Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. 
+    If not, print a different appropriate message.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int u_number, divident, divisor;
+
+    cout << 'Give me a randome number.';
+    cin >> u_number;
+    
+
+}
