7ba1bfd5c8ee778afb08a47b350463e8843d022c
Nu55k0pF <madironbrain@googlemail.com>
2023-01-26 20:47:41 +0100

    started work on the next challange

---
+++ String Lists/string_lists.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+
+    string uinput;
+    string rinput;
+
+    cout << "Write a palindrome:\n";
+    cin >> uinput;
+
+    for (string::reverse_iterator rit=uinput.rbegin(); rit!=uinput.rend(); ++rit){
+        // rinput.append(rit);
+        cout << *rit;
+
+    }
+    // compare returns an int. If the strings are equal compare will return 0
+    // if (uinput.compare(reverse_input) == 0){
+    //     cout << uinput + " is a palindrome.";
+    // }
+    // else {
+    //     cout << uinput + " is not a palindrome.";
+    // }
+    return 0;
+}
