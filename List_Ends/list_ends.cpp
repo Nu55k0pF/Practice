be11fba2fde174b327f728eacf1deeaab72bb340
Nu55k0pF <madironbrain@googlemail.com>
2023-02-01 22:38:35 +0100

    started work on list_ends.cpp

---
+++ List_Ends/list_ends.cpp
@@ -0,0 +1,19 @@
+/*
+Exercise 12 (and Solution)
+
+Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+Concepts to practice
+
+    Lists and properties of lists
+    List comprehensions (maybe)
+    Functions
+
+Since i feel, like my python skills have come back I will now focus on doing the C++ application first.
+I may feel like skipping the Python challange all together if I find it to easy. e.g. like this one
+*/
+
+
+
+int main(){
+
+}
