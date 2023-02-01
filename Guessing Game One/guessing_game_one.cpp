295af75ffb115be9a72abb0710bf11fd60135cea
Nu55k0pF <madironbrain@googlemail.com>
2023-02-01 12:58:32 +0100

    made the c++ program work

--- Guessing Game One/guessing_game_one.cpp
+++ Guessing Game One/guessing_game_one.cpp
@@ -14,12 +14,22 @@ Extras:
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <sstream>
+#include <limits>
 
 using namespace std;
 
 string check_game_end(){
-    cout << "'If you want to play another round press enter, if you want to exit type 'exit':";
     
+    string player_input = "play";
+    cout << "'If you want to play another round press enter, if you want to exit type 'exit':";
+    cin >> player_input;
+    if (player_input == "exit"){
+        return player_input;
+    }
+    else {
+        return "play";
+    }
 }
 
 int main(){
@@ -31,37 +41,53 @@ int main(){
 
     // Set gamestate
     string game = "play";
+    int trys = 0;
 
     cout << "Welcome! Lets play a guessing game.\n";
 
+    // Start the game loop
     while (game != "exit"){
 
         // Generate a pseudorandome number
-        int random_number = rand() % 10;
-        int trys = 0;
+        int random_number = rand() % 9 + 1;
         int user_number;
 
+        
+        // Get validated user input
+        while (true) {
         cout << "Guess a number from 1 to 9:\n";
-        cin >> user_number;
+        if (cin >> user_number) {
+            break;
+        } else {
+            cout << "Enter a valid integer value!\n";
+            cin.clear();
+            cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
 
+        // Check if the guess is correct
         if (user_number > random_number){
-            cout << "You guessed to high.";
+            cout << "You guessed to high.\n";
             ++trys;
         }
-        if else ( user_number < random_number){
-            cout << "You guessed to low.";
+        else if ( user_number < random_number){
+            cout << "You guessed to low.\n";
             ++trys;
         }
-        if else (user_number == random_number){
-            cout << "You guessed correctly!"
+        else if (user_number == random_number){
+            cout << "You guessed correctly! ";
             ++trys;
         }
         else {
-            cout << "Please guess a number from 1 to 9."
+            cout << "Please guess a number from 1 to 9.";
         }
-
+        // Ask the user for another round
+        game = check_game_end();
     }
-    
+
+    cout << "Thank you for playing, you guessed ";
+    cout << trys;
+    cout << " times";
 
     return 0;
 }
