e1f20649efa6663fabe98b2a473409356ee45d87
Nu55k0pF <madironbrain@googlemail.com>
2023-02-02 09:39:16 +0100

    started work on a randome vecror generator

--- List_Ends/list_ends.cpp
+++ List_Ends/list_ends.cpp
@@ -1,7 +1,9 @@
 /*
 Exercise 12 (and Solution)
 
-Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) 
+and makes a new list of only the first and last elements of the given list. 
+For practice, write this code inside a function.
 Concepts to practice
 
     Lists and properties of lists
@@ -17,19 +19,41 @@ I may feel like skipping the Python challange all together if I find it to easy.
 
 #include <vector>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 // Modules
 
 vector <int> vector_end(vector <int> input_vector){
-    
+  
+    /**
+     * Takes a vetcor containing integers and returning a vector with only the first and last
+     * element of the input vector.
+     * 
+     * Parameters:
+     * input_vector: vector of arbitrary lenth.
+     * 
+     * Returns:
+     * a vector containing the first and last element of the input vector.
+    */
+
+    vector <int> output_vector {input_vector.front(), input_vector.back()};
+
+    return output_vector;
+
 }
 
 
+vector <int> create_randome_vector(int range_of_numbers, int length_of_vector){
+
+    srand
+
+}
+
 int main(){
 
-    vector <int> a (5, 10, 15, 20, 25);
+    vector <int> a = {5, 10, 15, 20, 25};
     vector <int> b;
     
     b = vector_end(a);
