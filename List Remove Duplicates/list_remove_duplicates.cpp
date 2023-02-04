148806085173ebe7935929ca4d3ae25b30e51dd5
Nu55k0pF <madironbrain@googlemail.com>
2023-02-04 22:56:00 +0100

    made the first function work for c++

--- List Remove Duplicates/list_remove_duplicates.cpp
+++ List Remove Duplicates/list_remove_duplicates.cpp
@@ -11,6 +11,9 @@ Extras:
 */
 
 #include <vector>
+#include <set>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -18,7 +21,38 @@ using namespace std;
 /**
  * Remove duplicates with set
 */
-vector <int> remove_duplicates_1()
+vector <int> remove_duplicates_1(vector <int> input_vector)
 {
-    
+    set <int> temp (input_vector.begin(), input_vector.end()); 
+    vector <int> output_vector (temp.begin(), temp.end());
+    return output_vector;
+}
+
+vector <int> remove_duplicates_2(vector <int> input_vector)
+{
+    vector <int> output_vector;
+    for (int i : input_vector)
+    {
+    }
+    return output_vector;
+}
+
+void print_vector (vector <int> vector_2_print)
+{
+    for (int i : vector_2_print)
+    {
+        cout << i << ", ";
+    }
+}
+
+int main()
+{
+    vector <int> a = {1, 1, 2, 4, 5, 5};
+    vector <int> b (remove_duplicates_1(a));
+    vector <int> c (remove_duplicates_2(a));
+
+    print_vector(b);
+    print_vector(c);
+
+    return 0;
 }
