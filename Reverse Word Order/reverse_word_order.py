db9d9771fc8dce064fc89225e4c04fa129337677
Nu55k0pF <madironbrain@googlemail.com>
2023-02-08 20:27:55 +0100

    made a working python script

---
+++ Reverse Word Order/reverse_word_order.py
@@ -0,0 +1,40 @@
+"""
+Exercise 15
+
+Write a program (using functions!) that asks the user for a long string containing multiple words. 
+Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+
+-  My name is Michele
+
+Then I would see the string:
+
+- Michele is name My
+
+shown back to me.
+"""
+
+def reverse_string_1(string_to_reverse: str) -> str:
+    """ Takes a string and reverses it. """
+    reversed_wordorder = []
+    for word in string_to_reverse.split():
+        reversed_wordorder.append(word)
+    return " ".join(reversed_wordorder[::-1])
+
+
+def reverse_string_2(string_to_reverse: str) -> str:
+    """ Takes a string and reverses it. """
+    word_list = string_to_reverse.split()
+    word_list.reverse()
+    return " ".join(word_list)
+
+
+def main():
+    teststring = "My name is Michele"
+
+    print(reverse_string_1(teststring))
+    
+    print(reverse_string_2(teststring))
+
+
+if __name__ == "__main__":
+    main()
